bb1deacca53227fc620821cbf3a71239d003a1da
pieroBri <pierobri36@gmail.com>
2024-08-22 22:59:42 -0400

    Limpieza

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import Lobby from './components/Lobby';
-
-
 
 function App() {
     return (
@@ -9,7 +6,6 @@ function App() {
             <h2>Hola probando</h2>
             {/* <button onClick={ () => { socket.connect()}}>Conectar</button> */}
             <Lobby />
-            {/* Aquí es donde implementarás la lógica del juego */}
         </div>
     );
 }
