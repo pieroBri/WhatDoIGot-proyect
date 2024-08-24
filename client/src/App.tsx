53e95534121c45be1b8e92378d2605068ed86346
Daniel Duran Saavedra <danielduran.ads@gmail.com>
2024-08-24 02:29:04 -0400

    d

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import Lobby from './components/Lobby';
 function App() {
     return (
         <div className="App">
-            <h2>Hola probando</h2>
             {/* <button onClick={ () => { socket.connect()}}>Conectar</button> */}
             <Lobby />
         </div>
