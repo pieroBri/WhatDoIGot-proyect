8370328609f979f1a4ef363a01d9ad2a82c8e1a8
pieroBri <91641974+pieroBri@users.noreply.github.com>
2026-05-31 21:34:46 -0400

    Revert "Deploy docs branch (#7)"
    This reverts commit 8e8bc11b3873760d7245e3e2c1e7a2a02500ad5b.

--- swagger/api/index.js
+++ swagger/api/index.js
@@ -1,12 +1,11 @@
-// api/index.js - Vercel Serverless Function
+// api/index.js
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocumentation = require("../swagger_output.json");
 
 const app = express();
 
-// Swagger UI setup
+// Swagger UI setup - sirve en la raíz
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
 
-// Export for Vercel Serverless Functions
 module.exports = app;
